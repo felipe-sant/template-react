bc6b4086e2c23fcec64a9ed185ecaaf199e6b39a
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-06-27 22:02:14 -0300

    feat: adicionando exemplo de fetch

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,15 @@
+import buscarDados from "../functions/buscarDados"
+
 export default function Home() {
+    const pegarDados = async () => {
+        const result = await buscarDados('http://localhost:5000/Produtos')
+        console.log(result)
+    }
+
     return (
         <main>
             <h1>Hello world</h1>
+            <button onClick={pegarDados}>PEGAR DADOS</button>
         </main>
     )
 }
