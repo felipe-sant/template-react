46921dd5d3456afea1b880e3e6e630303080f433
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-06-28 00:03:58 -0300

    fix: arrumando erro de manifest e mudando nome do get

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,9 @@
-import buscarDados from "../functions/buscarDados"
-
-export default function Home() {
-    const pegarDados = async () => {
-        const result = await buscarDados('http://localhost:5000/Produtos')
-        console.log(result)
-    }
-
+function Home() {
     return (
         <main>
             <h1>Hello world</h1>
-            <button onClick={pegarDados}>PEGAR DADOS</button>
         </main>
     )
 }
+
+export default Home
