05db3d48af046b46726b6de79230477a9e35beab
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-07-23 18:53:00 -0300

    feat: adicionando hooks adicionais

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-import useAppDispatch from "../hooks/useAppDispatch"
 import css from "../styles/pages/home.module.css"
 
 function Home() {
-    const dispatch = useAppDispatch()
-
     return (
         <main className={css.main}>
-            <h1>Hello world</h1>
+            <h1>Hello World!</h1>
         </main>
     )
 }
